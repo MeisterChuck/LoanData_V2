855c2435f85bb16bc2f037ad15cbc74d95fb9f73
Michael <turner.turnermichael@gmail.com>
2016-06-09 15:19:29 -0400

    Completed function MakePayment() and fixed variable A within constructor LoanData

--- LoanDataRevisedCSharp/Program.cs
+++ LoanDataRevisedCSharp/Program.cs
@@ -8,48 +8,73 @@ namespace LoanDataRevisedCSharp
         
         public LoanData(double loanAmount, double years, double interestRate)
         {
-            this.i = interestRate / 1200;
-            this.n = years * 12;
-            this.A = loanAmount * ((i * Math.Pow(1 + i, n)) - 1);
-            this.Bal = loanAmount;
+            i = interestRate / 1200;
+            n = years * 12;
+            A = loanAmount * ((i * Math.Pow(1 + i, n)) / ((Math.Pow(1 + i, n)) - 1));
+            Bal = loanAmount;
         }
 
         public void MakePayment(double payment)
         {
-            this.Bal = (i + 1) * Bal - payment;
+            Bal = (i + 1) * Bal - payment;
         }
 
-        //Debugging
-        public void Print()
+        public void PrintAmortizationSchedule()
         {
-            System.Console.WriteLine(Bal);
-            System.Console.WriteLine(n);
-            System.Console.WriteLine(i);
-        }
+            double payment, interestPaid, principlePaid, endingBalance;
+            double beginningBalance = Bal;
+            String title = String.Format("{0, 17}   {1, 13}   {2, 14}   {3, 14}\n",
+                                         "Beginning Balance",
+                                         "Interest Paid",
+                                         "Principle Paid",
+                                         "Ending Balance");
 
+            Console.Write("\n" + title);
+
+            while(beginningBalance > 0)
+            {
+                if (i + 1 * Bal > A)
+                {
+                    payment = A;
+                }
+                else
+                {
+                    payment = (i + 1) * Bal;
+                }
+
+                interestPaid = i * beginningBalance;
+                principlePaid = A - interestPaid;
+                endingBalance = (i + 1) * beginningBalance - A;
+
+                title = String.Format("{0, 17:C2}   {1, 13:C2}   {2, 14:C2}   {3, 14:C2}\n",
+                                         beginningBalance,
+                                         interestPaid,
+                                         principlePaid,
+                                         endingBalance);
+                Console.Write(title); 
+
+                beginningBalance = endingBalance;
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            int loanAmount, years, interestRate;
+            double loanAmount, years, interestRate;
             LoanData loanData;
 
             System.Console.Write("Loan amount is: ");
-            loanAmount = int.Parse(System.Console.ReadLine());
+            loanAmount = double.Parse(System.Console.ReadLine());
             System.Console.Write("Number of years to pay off loan is: ");
-            years = int.Parse(System.Console.ReadLine());
+            years = double.Parse(System.Console.ReadLine());
             System.Console.Write("Yearly interest rate is: ");
-            interestRate = int.Parse(System.Console.ReadLine());
-
-            //Debugging
-            System.Console.WriteLine(loanAmount);
-            System.Console.WriteLine(years);
-            System.Console.WriteLine(interestRate);
+            interestRate = double.Parse(System.Console.ReadLine());
 
             loanData = new LoanData(loanAmount, years, interestRate);
-            loanData.Print();
+            loanData.MakePayment(650.00);
+            loanData.PrintAmortizationSchedule();
         }
     }
 }
