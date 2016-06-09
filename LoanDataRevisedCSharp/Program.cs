f1aa82f2d1be296838a7a63a32b1486582a70f81
Michael <turner.turnermichael@gmail.com>
2016-06-09 11:03:36 -0400

    Created constructor LoanData and function MakePayment

--- LoanDataRevisedCSharp/Program.cs
+++ LoanDataRevisedCSharp/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace LoanDataRevisedCSharp
 {
+    class LoanData
+    {
+        private double Bal, n, i, A;
+        
+        public LoanData(double loanAmount, double years, double interestRate)
+        {
+            this.i = interestRate / 1200;
+            this.n = years * 12;
+            //this.A; Gotta figure out how to create powers
+            this.Bal = loanAmount;
+        }
+
+        public void MakePayment(double payment)
+        {
+            this.Bal = (i + 1) * Bal - payment;
+        }
+
+        //Debugging
+        public void Print()
+        {
+            System.Console.WriteLine(Bal);
+            System.Console.WriteLine(n);
+            System.Console.WriteLine(i);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             int loanAmount, years, interestRate;
+            LoanData loanData;
 
             System.Console.Write("Loan amount is: ");
             loanAmount = int.Parse(System.Console.ReadLine());
@@ -17,6 +45,9 @@
             System.Console.WriteLine(loanAmount);
             System.Console.WriteLine(years);
             System.Console.WriteLine(interestRate);
+
+            loanData = new LoanData(loanAmount, years, interestRate);
+            loanData.Print();
         }
     }
 }
