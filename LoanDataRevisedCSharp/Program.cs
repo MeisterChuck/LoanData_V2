1f1cdfbb8cc8e09ece3b76a96eefcb0bc57772fc
Michael <turner.turnermichael@gmail.com>
2016-06-09 10:43:58 -0400

    Created the main class

--- LoanDataRevisedCSharp/Program.cs
+++ LoanDataRevisedCSharp/Program.cs
@@ -10,6 +10,19 @@ namespace LoanDataRevisedCSharp
     {
         static void Main(string[] args)
         {
+            int loanAmount, years, interestRate;
+
+            System.Console.Write("Loan amount is: ");
+            loanAmount = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Number of years to pay off loan is: ");
+            years = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Yearly interest rate is: ");
+            interestRate = int.Parse(System.Console.ReadLine());
+
+            //Debugging
+            System.Console.WriteLine(loanAmount);
+            System.Console.WriteLine(years);
+            System.Console.WriteLine(interestRate);
         }
     }
 }
